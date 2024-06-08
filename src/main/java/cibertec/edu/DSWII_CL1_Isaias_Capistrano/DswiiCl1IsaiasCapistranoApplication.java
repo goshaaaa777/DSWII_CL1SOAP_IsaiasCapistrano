f399cd48a3a8bbb0e14647135590712702beaba4
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-08 09:53:32 -0500

    inicio de examen cl1

---
+++ src/main/java/cibertec/edu/DSWII_CL1_Isaias_Capistrano/DswiiCl1IsaiasCapistranoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.DSWII_CL1_Isaias_Capistrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1IsaiasCapistranoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1IsaiasCapistranoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/DSWII_CL1_Isaias_Capistrano/DswiiCl1IsaiasCapistranoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.DSWII_CL1_Isaias_Capistrano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1IsaiasCapistranoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
