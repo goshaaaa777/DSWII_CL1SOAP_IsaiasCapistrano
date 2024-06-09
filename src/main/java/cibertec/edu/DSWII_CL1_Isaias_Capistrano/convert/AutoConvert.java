55ea7321dc4a739c8940ebb86e380cb885915f79
goshaaaa777 <isaiasddx322z@gmail.com>
2024-06-08 21:49:47 -0500

    Carga controller gsh

---
+++ src/main/java/cibertec/edu/DSWII_CL1_Isaias_Capistrano/convert/AutoConvert.java
@@ -0,0 +1,2 @@
+package cibertec.edu.DSWII_CL1_Isaias_Capistrano.convert;public class AutoConvert {
+}
---
+++ src/main/java/cibertec/edu/DSWII_CL1_Isaias_Capistrano/model/dto/AutorDto.java
@@ -0,0 +1,4 @@
+package cibertec.edu.DSWII_CL1_Isaias_Capistrano.model.dto;
+
+public class AutorDto {
+}
